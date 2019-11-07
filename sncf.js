242dc7ca7d31e49b59db3a8db4a4088d3312b26b
jphayek <jphayek1@gmail.com>
2019-11-07 10:43:20 +0100

    SNCF

---
+++ sncf.js
@@ -0,0 +1,15 @@
+var sncf = [
+	{ ville: 'Paris-Lyon', trajet: 'Lyon', duree: 75 },
+	{ ville: 'Paris-Lyon', trajet: 'Paris-Nord', duree: 15 },
+	{ ville: 'Paris-Lyon', trajet: 'Mompellier', duree: 125 },
+	{ ville: 'Paris-Lyon', trajet: 'Paris-Monparnasse', duree: 15 },
+	{ ville: 'Paris-Monparnasse', trajet: 'Rennes', duree: 125 },
+	{ ville: 'Paris-Monparnasse', trajet: 'Bordeaux', duree: 180 },
+	{ ville: 'Paris-Monparnasse', trajet: 'Paris-Nord', duree: 15 },
+	{ ville: 'Paris-Monparnasse', trajet: 'Rennes', duree: 125 },
+	{ ville: 'Rennes', trajet: 'Brest', duree: 75 },
+	{ ville: 'Bordeaux', trajet: 'Toulouse', duree: 180 },
+	{ ville: 'Toulouse', trajet: 'Mompellier', duree: 240 },
+	{ ville: 'Lyon', trajet: 'Marseille', duree: 90 },
+	{ ville: 'Paris-Nord', trajet: 'Londres', duree: 150 }
+];
